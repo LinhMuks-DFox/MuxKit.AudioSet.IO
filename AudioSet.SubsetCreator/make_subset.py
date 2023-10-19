72e615435339ceb5dec04f3e7c0ef800364b4b3c
Mux <muxxum65536@gmail.com>
2023-10-19 15:13:10 +0900

    sub set creator added

--- AudioSet.SubsetCreator/make_subset.py
+++ AudioSet.SubsetCreator/make_subset.py
@@ -1,3 +1,84 @@
 import json
+import os
 import shutil
 from subset_maker_config import *
+
+"""
+Dataset structure:
+    "0": {
+        "path": "eval_segments_4000.csv.splits/--4gqARaEJE.wav",
+        "onto": [
+            "/m/068hy",
+            "/m/07q6cd_",
+            "/m/0bt9lr",
+            "/m/0jbk"
+        ],
+        "label_digits": [
+            "73",
+            "361",
+            "74",
+            "72"
+        ],
+        "label_display": [
+            "Domestic animals, pets",
+            "Squeak",
+            "Dog",
+            "Animal"
+        ]
+    }
+"""
+
+with open(PATH_TO_TRAIN_JSON, "r") as t, open(PATH_TO_EVAL_PATH, "r") as e:
+    train_data_json = json.load(t)
+    eval_data_json = json.load(e)
+
+sub_train_json = {}
+sub_train_statistic = {
+    key: 0 for key in SELECTED_LABELS.keys()
+}
+sub_eval_json = {}
+sub_eval_statistic = {
+    key: 0 for key in SELECTED_LABELS.keys()
+}
+
+
+def make_json(data_json, sub_json, sub_statistic):
+    iota = 0
+    for key, value in data_json.items():
+        if len(union := (set(value["label_digits"]) & set(SELECTED_LABELS.keys()))) >= 1:
+            union = list(union)[0]
+            sub_statistic[union] += 1
+            sub_json[iota] = {
+                "path": value["path"],
+                "onto": SELECTED_LABELS[union]["onto"],
+                "label_digits": SELECTED_LABELS[union]["name"],
+            }
+            print(f"Added {key}({value['path']}, {value['label_display']}) to subset.")
+            print(f"Union: {union}")
+            iota += 1
+
+
+with open("sub_train.json", "w") as st, open("sub_eval.json", "w") as se, open("description.txt", "w") as d:
+    make_json(train_data_json, sub_train_json, sub_train_statistic)
+    make_json(eval_data_json, sub_eval_json, sub_eval_statistic)
+    json.dump(sub_train_json, st, indent=4)
+    json.dump(sub_eval_json, se, indent=4)
+
+    for k, v in SELECTED_LABELS.items():
+        d.write("label {}: \n\tonto: {}\n\tdisplay name: {}\n\tsample count(train:{}, eval:{})\n".format(k, v["onto"],
+                                                                                                         v["name"],
+                                                                                                         sub_train_statistic[
+                                                                                                             k],
+                                                                                                         sub_eval_statistic[
+                                                                                                             k]))
+
+    d.write("Train dataset length: {}\n".format(len(sub_train_json)))
+    d.write("Eval dataset length: {}\n".format(len(sub_eval_json)))
+
+# move files
+if os.path.exists("subset_json"):
+    shutil.rmtree("subset_json")
+os.mkdir("subset_json")
+shutil.move("sub_train.json", "subset_json")
+shutil.move("sub_eval.json", "subset_json")
+shutil.move("description.txt", "subset_json")
--- AudioSet.SubsetCreator/subset_maker_config.py
+++ AudioSet.SubsetCreator/subset_maker_config.py
@@ -1,5 +1,24 @@
-PATH_TO_AUDIO_JSON: str = r""
+from typing import Dict
+import pathlib
+
+PATH_TO_TRAIN_JSON: pathlib.Path = pathlib.Path(r"F:\DataSets\Audioset\balanced\segments\AudioSet.json")
+PATH_TO_EVAL_PATH: pathlib.Path = pathlib.Path(r"F:\DataSets\Audioset\eval\segments\AudioSet.json")
 DRY_RUN: bool = False
 SUB_SET_SAMPLE_COUNT: int = 20000 // 10
 PACK: bool = True
 RANDOM_SEED: int = 42
+
+SELECTED_LABELS: Dict[str, Dict] = {
+    "2": {"name": "Female Speech", "onto": "/m/02zsn"},  # 50
+    "141": {"name": "Electric guitar", "onto": "/m/02sgy"},  # 60
+    "351": {"name": "Engine staring", "onto": "/t/dd00130"},  # 55
+    "352": {"name": "idling", "onto": "/m/07pb8fc"},  # 58
+    "395": {"name": "Alarm clock", "onto": "/m/046dlr"},  # 50
+    "426": {"name": "Explosion", "onto": "/m/014zdl"},  # 53
+    "119": {"name": "Owl", "onto": "/m/09d5_"},  # 57
+    "122": {"name": "Canidae, dogs, wolve", "onto": "/m/01z5f"},  # 53
+    "287": {"name": "Thunder", "onto": "/m/0ngt1"},  # 57
+    "294": {"name": "Ocean", "onto": "/m/05kq4"},  # 64
+}
+
+SINGLE_LABEL: bool = True
